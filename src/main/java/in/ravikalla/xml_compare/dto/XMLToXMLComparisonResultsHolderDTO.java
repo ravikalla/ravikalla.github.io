6c838336632e35ba06ff8429e02d096e80fdba81
ravi523096 <ravi2523096@gmail.com>
2017-02-25 12:46:30 -0500

    removing unnecessary files

--- src/main/java/in/ravikalla/xml_compare/dto/XMLToXMLComparisonResultsHolderDTO.java
+++
@@ -1,8 +0,0 @@
-package in.ravikalla.xml_compare.dto;
-
-import java.util.List;
-
-public class XMLToXMLComparisonResultsHolderDTO {
-	public List<String> lstMismatchedDataForCSV;
-	public List<String> lstMatchedDataForCSV;
-}
